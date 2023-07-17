e1bc417268c2da84cf4c3dd6be1f7b3dff3e061d
mbeyzademirkubuz <96108245+mbeyzademirkubuz@users.noreply.github.com>
2023-07-17 21:41:36 +0300

    Bütçe Takibi Websitesi
    Bütçe takibi yapmak için kullanılabilecek, database bağlantısı olan bir web sitesidir.

---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Bir kategori seçin.")]
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Tutar, 0'dan büyük olmalıdır.")]
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Type == "Expense") ? "- " : "+ ") + Amount.ToString("C0");
+            }
+        }
+
+    }
+}
